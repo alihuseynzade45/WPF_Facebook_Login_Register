446980dc0e96f1a430fbcabdb6ee984c1be1469b
Standard Computer's <Standard Computer's@DESKTOP-FRC77TS>
2023-03-10 17:42:21 +0300

    Proje dosyası ekle.

---
+++ WPF_Login_Register/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF_Login_Register
+{
+    /// <summary>
+    /// MainWindow.xaml etkileşim mantığı
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+
+
+  
+        private void NewWindow_Click(object sender, RoutedEventArgs e)
+        {
+            Window1 Resgister = new Window1();
+            Resgister.Show();
+        }
+
+
+        private void textBox_PreviewMouseDown(object sender, RoutedEventArgs e)
+        {
+            SifreLabel.Content = "";
+        }
+
+    }
+
+}
+
---
+++ WPF_Login_Register/Window1.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WPF_Login_Register
+{
+    /// <summary>
+    /// Window1.xaml etkileşim mantığı
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        public Window1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            ComboBox comboBox = sender as ComboBox;
+            ComboBoxItem selectedItem = comboBox.SelectedItem as ComboBoxItem;
+
+            if (selectedItem != null)
+            {
+
+                gun.Items.Clear();
+                gun.Items.Add(selectedItem.Content.ToString());
+            }
+        }
+
+
+
+        private void textBox_PreviewMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            sifreLabel.Content = "";
+        }
+    }
+}
